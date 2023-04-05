e9ec1b2251be7de18e4a2de9371f9e6f9e1ff15b
yasyir90 <yasyirmasyal@gmail.com>
2023-04-05 21:57:32 +0700

    add card dan membuat mekanisme pencarian

--- src/App.js
+++ src/App.js
@@ -1,49 +1,23 @@
 import "./App.css"
 // import {getMovieList} from "./api"
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import Card from "./components/card";
 import Wrapper from "./components/wrapper";
 import Header from "./components/header"
-import Search from "./components/search";
+import SearchMovie from "./components/search";
 
 function App() {
 
-
-  // const [popularMove, setPopularMovies] = useState([])
-
-
-  // useEffect(() => {
-  //   getMovieList().then((result) => {
-  //     setPopularMovies(result)
-  //   })
-  // },[]
-  // )
   const name = 'yasyir';
-  // console.log({popularMove : popularMove})
 
-  const [PopularMovies, setPopularMovies] = useState([]);
-
-  useEffect(() => {
-    const getMovieList = async () => {
-      const movie = await axios.get('https://api.themoviedb.org/3/movie/popular?api_key=36bc3a789b7ad9af4dd3bd9d3f3df68a');
-      return movie.data.results
-    };
-
-    getMovieList().then((result) =>{
-      setPopularMovies(result)
-    })
-  }, []);
-  
-console.log({PopularMovies : PopularMovies})
   
   return (
   <div>
     <div className="container-body">
+    <Header nama={name}/>
+        <SearchMovie />
+        <Wrapper />
       <div className="movie-container">
-        <Header nama={name}/>
-        <Search />
-       <Wrapper />
-       <Wrapper />
+        <Card />
       </div>
     </div>
   </div>
--- src/components/search.js
+++ src/components/search.js
@@ -1,12 +1,22 @@
 import React from 'react'
+import axios from 'axios';
 
-function Search(){
-    const search = (Q) =>{
-        console.log({Q})
+function SearchMovie(){
+    const search =async (q) =>{
+       if(q.length > 3){
+        const searchMovie = await axios.get(`https://api.themoviedb.org/3/search/movie?query=${q}&api_key=36bc3a789b7ad9af4dd3bd9d3f3df68a`)
+        console.log({searchMovie:searchMovie})
+        console.log(searchMovie.request)
+       }
     }
 
-    
 
+    // const searchMovieFinish = async (q) =>{
+    //     if(q.length > 3){
+    //         const query = await.searchMovie(q)
+    //         console.log({query : query})
+    //     }
+    // }
     return(
         <div className='search-container'>
             <input placeholder='cari film...'
@@ -15,4 +25,4 @@ function Search(){
     );
 }
 
-export default Search;
+export default SearchMovie;
