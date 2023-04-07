a80aadfb9e62865768090be5677e2e47e153ad2c
yasyir90 <yasyirmasyal@gmail.com>
2023-04-07 11:40:14 +0700

    fix search and add style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import "./App.css"
 // import {getMovieList} from "./api"
-import Card from "./components/card";
-import Wrapper from "./components/wrapper";
-import Header from "./components/header"
-import SearchMovie from "./components/search";
+import React, { useState, useEffect } from 'react';
+import axios from "axios";
+import { searchMovie } from "./api";
+
 
 function App() {
+  
+  const [PopularMovies, setPopularMovies] = useState([]);
+  useEffect(() => {
+      const getMovieList = async () => {
+        const movie = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=36bc3a789b7ad9af4dd3bd9d3f3df68a`);
+        return movie.data.results
+      };
+  
+      getMovieList().then((result) =>{
+        setPopularMovies(result)
+      })
+    }, []);
 
-  const name = 'yasyir';
+
+    const PopularMoviesList =() =>{
+      return PopularMovies.map((movie, i) =>{
+        return(
+                  <div className='container-card' key={i}>
+            <div className='title'>{movie.title}</div>
+            <img className='picture' src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}` }/>
+            <div className='date'>{movie.release_date}</div>
+            <div className='rate'>{movie.vote_average}</div>
+           </div>
+        )
+      })
+    }
+
+
+
+  const search = async (q) => {
+    if(q.length > 3){
+      const query = await searchMovie(q)
+      setPopularMovies(query)
+    }
+  }
+  
 
   
   return (
   <div>
     <div className="container-body">
-    <Header nama={name}/>
-        <SearchMovie />
-        <Wrapper />
+      <div className="search-btn">
+      <input placeholder='cari film...'
+            onChange={({target}) => search(target.value)}/>
+      </div>
       <div className="movie-container">
-        <Card />
+        <PopularMoviesList />
       </div>
     </div>
   </div>
--- src/api.js
+++ src/api.js
@@ -1,16 +1,13 @@
-// import axios from 'axios'
+import axios from 'axios'
 
-// const apiKey = process.env.REACT_APP_BASEURL
-// const baseUrl = process.env.REACT_APP_APIKEY
+export const getMovieList = async() =>{
+    const movie = await axios.get(`https://api.themoviedb.org/3/movie/popular?page=1&api_key=36bc3a789b7ad9af4dd3bd9d3f3df68a`)
+     console.log({MovieList : movie})
+     console.log({list : movie})
+    return movie.data.results
+}
 
-// export const getMovieList = async() =>{
-//     const movie = await axios.get(`${baseUrl}/movie/popular?api_key=${apiKey}`)
-//      console.log({MovieList : movie})
-//      console.log({list : movie})
-//     return movie.data.results
-// }
-
-// export const searchMovie = async(q) =>{
-//     const search = await axios.get(q)
-//     return
-// }
+export const searchMovie = async(q) =>{
+    const search = await axios.get(`https://api.themoviedb.org/3/search/movie?query=${q}&page=1&api_key=36bc3a789b7ad9af4dd3bd9d3f3df68a`)
+    return search.data.results
+}
