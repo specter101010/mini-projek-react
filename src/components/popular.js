0f6d6f32dc2cd1c578b484ba7b688d49ccc1f394
yasyir90 <yasyirmasyal@gmail.com>
2023-04-11 13:21:01 +0700

    add search in popular movies

--- src/components/popular.js
+++ src/components/popular.js
@@ -13,6 +13,8 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import './popular.css'
+import { searchMovie } from '../api';
+
 
 const PopularMovies = () => {
   const [popularMovies, setPopularMovies] = useState([]);
@@ -52,58 +54,84 @@ const PopularMovies = () => {
     setExpanded(expandedCopy);
   };
 
-  return popularMovies.map((movie, i) => {
-    return (
-        <div className='container-card' key={i}>
-        <Card sx={{ maxWidth: 350, backgroundColor: '#1f2022' }}>
-          <CardHeader
-            title={
-              <Typography variant="h6" component="div" style={{ color: '#fff', fontSize: '1.5rem' }}>
-                {movie.title}
-              </Typography>
-            }
-            subheader={movie.release_date}
-          />
-          <CardMedia
-            component="img"
-            height="194"
-            image={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-            alt="Paella dish"
-             style={{ height: "inherit", width: "100%", objectFit: "cover" }}
-          />
-          <CardContent>
-            <Typography variant="body2" color="text.secondary" style={{color: '#fff'}}>
-            </Typography>
-          </CardContent>
-          <CardActions disableSpacing>
-            <IconButton aria-label="add to favorites" style={{color: '#fff'}}>
-              <FavoriteIcon />
-            </IconButton>
-            <IconButton aria-label="share" style={{color: '#fff'}}>
-              <ShareIcon />
-            </IconButton>
-            <ExpandMore
-              expand={expanded[i]}
-              onClick={() => handleExpandClick(i)}
-              aria-expanded={expanded[i]}
-              aria-label="show more"
-            >
-              <ExpandMoreIcon />
-            </ExpandMore>
-          </CardActions>
-          <Collapse in={expanded[i]} timeout="auto" unmountOnExit>
-            <CardContent>
-              <Typography paragraph style={{color: '#fff'}}>Method:</Typography>
-              <Typography paragraph style={{color: '#fff'}}>
-                {movie.overview}
-              </Typography>
-            </CardContent>
-          </Collapse>
+  const [searchQuery, setSearchQuery] = useState('');
 
-        </Card>
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+  
+  const handleSearchSubmit = async () => {
+    if (searchQuery.length > 3) {
+      const query = await searchMovie(searchQuery);
+      setPopularMovies(query);
+    }
+  };
+
+
+ 
+  return(
+    <div>
+           <div className='search-container'>
+        <input type='text' value={searchQuery} onChange={handleSearchChange} />
+        <button onClick={handleSearchSubmit}>Search</button>
       </div>
-    );
-  });
+
+      <div className='container-card'>
+        {popularMovies.map((movie, i)=>{
+            return(
+                <Card sx={{ maxWidth: 350, backgroundColor: '#1f2022' }}>
+                <CardHeader
+                  title={
+                    <Typography variant="h6" component="div" style={{ color: '#fff', fontSize: '1.5rem' }}>
+                      {movie.title}
+                    </Typography>
+                  }
+                  subheader={movie.release_date}
+                />
+                <CardMedia
+                  component="img"
+                  height="194"
+                  image={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                  alt="Paella dish"
+                   style={{ height: "inherit", width: "100%", objectFit: "cover" }}
+                />
+                <CardContent>
+                  <Typography variant="body2" color="text.secondary" style={{color: '#fff'}}>
+                  </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                  <IconButton aria-label="add to favorites" style={{color: '#fff'}}>
+                    <FavoriteIcon />
+                  </IconButton>
+                  <IconButton aria-label="share" style={{color: '#fff'}}>
+                    <ShareIcon />
+                  </IconButton>
+                  <ExpandMore
+                    expand={expanded[i]}
+                    onClick={() => handleExpandClick(i)}
+                    aria-expanded={expanded[i]}
+                    aria-label="show more"
+                  >
+                    <ExpandMoreIcon />
+                  </ExpandMore>
+                </CardActions>
+                <Collapse in={expanded[i]} timeout="auto" unmountOnExit>
+                  <CardContent>
+                    <Typography paragraph style={{color: '#fff'}}>Method:</Typography>
+                    <Typography paragraph style={{color: '#fff'}}>
+                      {movie.overview}
+                    </Typography>
+                  </CardContent>
+                </Collapse>
+      
+              </Card>
+            )
+        })}
+     
+      </div>
+           
+    </div>
+  )
 };
 
 export default PopularMovies;
---
+++ src/components/search.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import axios from "axios";
+
+const SearchFilm =() =>{
+
+    
+
+    return(
+        <div>
+
+        </div>
+    )
+}
