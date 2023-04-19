e60e021deb26521cf46ec9a7bf14ec4bdafdb803
yasyir90 <yasyirmasyal@gmail.com>
2023-04-19 10:28:33 +0700

    add content footer

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css'
 import TrendingMovies from "./components/trending";
 import UpComingMovies from "./components/upcoming";
 import { Layout, Row, Col } from 'antd';
-import { PhoneOutlined, MailOutlined, FacebookOutlined, InstagramOutlined, TwitterOutlined } from '@ant-design/icons';
+import { PhoneOutlined, MailOutlined, FacebookOutlined, InstagramOutlined, GithubOutlined, LinkedinOutlined } from '@ant-design/icons';
 
 
 function App() {
@@ -23,25 +23,24 @@ function App() {
         </div>      
       </Container>
       <Layout.Footer style={{ width: '100%' }}>
-  <div className="footer">
-    <Row>
-      <Col span={8} offset={4}>
-        <h4><PhoneOutlined /> Kontak</h4>
-        <a href="tel:123456789"><PhoneOutlined /> 123456789</a>
-        <a href="mailto:info@example.com"><MailOutlined /> info@example.com</a>
-      </Col>
-      <Col span={8} offset={4}>
-        <h4><FacebookOutlined /> Sosial Media</h4>
-        <a href="https://facebook.com/example"><FacebookOutlined /> @example</a>
-        <a href="https://instagram.com/example"><InstagramOutlined /> @example</a>
-        <a href="https://twitter.com/example"><TwitterOutlined /> @example</a>
-      </Col>
-    </Row>
-    <p style={{ textAlign: 'center' }}>© Yasyir Masy'al 2023. All rights reserved.</p>
-  </div>
-</Layout.Footer>
-
-
+        <div className="footer">
+          <Row>
+            <Col span={8} offset={4}>
+              <h4><PhoneOutlined /> Kontak</h4>
+              <a href="tel:08982228178"><PhoneOutlined />0898-222-8178</a>
+              <a href="yasyirmasyal@gmail.com"><MailOutlined /> yasyirmasyal@gmail.com</a>
+            </Col>
+            <Col span={8} offset={4}>
+              <h4><FacebookOutlined /> Sosial Media</h4>
+              <a href="https://www.facebook.com/yasir.masyal.5?mibextid=ZbWKwL"><FacebookOutlined />Yasir Masy'al</a>
+              <a href="https://instagram.com/lauliet_y?igshid=ZDdkNTZiNTM="><InstagramOutlined />@LAULIET_Y</a>
+              <a href="https://github.com/yasyir90"><GithubOutlined />yasyir90</a>
+              <a href="https://www.linkedin.com/in/yasyir-masy-al"><LinkedinOutlined />Yasyir Masy'al</a>
+            </Col>
+          </Row>
+          <p style={{ textAlign: 'center' }}>© Yasyir Masy'al 2023. All rights reserved.</p>
+        </div>
+      </Layout.Footer>
     </div>
   );
 }
